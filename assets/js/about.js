ae0e034232bc53b963413a2e34185e3b1d580dad
EdwardNew <pkarrows@gmail.com>
2022-09-11 02:00:10 -0700

    STABLE SITE: last commit on stable site as a monorepo, migrating new mobile update to remote theme

--- assets/js/about.js
+++
@@ -1,8 +0,0 @@
-var fade_in = anime({
-  targets: '.fade-in, .content-box',
-  translateY: [-5, 0],
-  opacity: [0, 1],
-  delay: anime.stagger(50, {start: 100}),
-  duration: 1000,
-  easing: 'easeOutQuad'
-})    
